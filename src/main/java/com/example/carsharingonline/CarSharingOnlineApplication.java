e1ed8e8a91ba7e2b6ab805befa3b2d06f963a9d8
Olga Starikova <ole4kastar79@gmail.com>
2025-01-19 00:23:01 +0200

    first commit

---
+++ src/main/java/com/example/carsharingonline/CarSharingOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.carsharingonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingOnlineApplication.class, args);
+    }
+
+}
